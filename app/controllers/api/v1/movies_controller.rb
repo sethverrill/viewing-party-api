3687123c8bd5062b2ffa9c9258c674b5ff660fa9
sethverrill <verrill.seth@gmail.com>
2024-12-05 13:02:29 -0700

    reverting code now that Heroku issue resolved

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,71 +1,71 @@
 class Api::V1::MoviesController < ApplicationController
-#   def index
-#     api = Rails.application.credentials.dig(:tmdb, :api_read_access_token)
-#     query = params[:query]
-
-#     conn = Faraday.new(url: "https://api.themoviedb.org/3") do |f|
-#       f.headers['Authorization'] = "Bearer #{api}"
-#       f.headers['accept'] = 'application/json'
-#     end
-
-#     if query.present?
-#       response = conn.get("search/movie", {
-#         query: query,
-#         include_adult: false,
-#         language: 'en-US',
-#         page: 1
-#       })
-#     else
-#       response = conn.get("movie/top_rated", {
-#         language: 'en-US',
-#         page: 1
-#       })
-#     end
-
-#     if response.success?
-#       json = JSON.parse(response.body, symbolize_names: true)
-#       movies = json[:results].first(20)
-#       render json: MovieSerializer.new(movies).serializable_hash.to_json
-#     else
-#       render json: { error: "Failed to fetch movies: #{response.status}" }, status: :bad_request
-#     end
-#   end
-  before_action :tmdb_connection
   def index
-    response = @conn.get('movie/top_rated', language: 'en-US', page: 1)
+    api = Rails.application.credentials.dig(:tmdb, :api_read_access_token)
+    query = params[:query]
+
+    conn = Faraday.new(url: "https://api.themoviedb.org/3") do |f|
+      f.headers['Authorization'] = "Bearer #{api}"
+      f.headers['accept'] = 'application/json'
+    end
+
+    if query.present?
+      response = conn.get("search/movie", {
+        query: query,
+        include_adult: false,
+        language: 'en-US',
+        page: 1
+      })
+    else
+      response = conn.get("movie/top_rated", {
+        language: 'en-US',
+        page: 1
+      })
+    end
 
     if response.success?
       json = JSON.parse(response.body, symbolize_names: true)
       movies = json[:results].first(20)
       render json: MovieSerializer.new(movies).serializable_hash.to_json
     else
-      render json: { error: 'Failed to fetch top-rated movies' }, status: response.status
+      render json: { error: "Failed to fetch movies: #{response.status}" }, status: :bad_request
     end
   end
+#   before_action :tmdb_connection
+#   def index
+#     response = @conn.get('movie/top_rated', language: 'en-US', page: 1)
 
-  def search
-    query = params[:query]
+#     if response.success?
+#       json = JSON.parse(response.body, symbolize_names: true)
+#       movies = json[:results].first(20)
+#       render json: MovieSerializer.new(movies).serializable_hash.to_json
+#     else
+#       render json: { error: 'Failed to fetch top-rated movies' }, status: response.status
+#     end
+#   end
 
-    response = @conn.get('search/movie', {
-      query: query,
-      include_adult: false,
-      language: 'en-US',
-      page: 1
-    })
+#   def search
+#     query = params[:query]
 
-    json = JSON.parse(response.body, symbolize_names: true)
-    movies = json[:results].first(20)
-    render json: MovieSerializer.new(movies).serializable_hash.to_json
-  end
+#     response = @conn.get('search/movie', {
+#       query: query,
+#       include_adult: false,
+#       language: 'en-US',
+#       page: 1
+#     })
 
-  private
-  def tmdb_connection
-    api = Rails.application.credentials.dig(:tmdb, :api_read_access_token)
-    @conn = Faraday.new(url: "https://api.themoviedb.org/3") do |f|
-      f.headers['Authorization'] = "Bearer #{api}"
-      f.headers['accept'] = 'application/json'
-    end
-  end
-end
+#     json = JSON.parse(response.body, symbolize_names: true)
+#     movies = json[:results].first(20)
+#     render json: MovieSerializer.new(movies).serializable_hash.to_json
+#   end
+
+#   private
+#   def tmdb_connection
+#     api = Rails.application.credentials.dig(:tmdb, :api_read_access_token)
+#     @conn = Faraday.new(url: "https://api.themoviedb.org/3") do |f|
+#       f.headers['Authorization'] = "Bearer #{api}"
+#       f.headers['accept'] = 'application/json'
+#     end
+#   end
+# end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create, :index]
       resources :sessions, only: :create
-      resources :movies, only: [:index] do
-        collection do
-          get 'search'
-        end
-      end
+      resources :movies, only: [:index]
     end
   end
 end
