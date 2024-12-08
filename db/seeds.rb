816ce888e15fa1ad17060319e82b45bafc6abd91
sethverrill <verrill.seth@gmail.com>
2024-12-08 10:51:52 -0700

    Seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-User.create!(name: "Danny DeVito", username: "danny_de_v", password: "jerseyMikesRox7")
-User.create!(name: "Dolly Parton", username: "dollyP", password: "Jolene123")
-User.create!(name: "Lionel Messi", username: "futbol_geek", password: "test123")
+user1 = User.find_or_create_by!(name: "Danny DeVito", username: "danny_de_v") do |user|
+  user.password = "jerseyMikesRox7"
+end
+user2 = User.find_or_create_by!(name: "Dolly Parton", username: "dollyP") do |user|
+  user.password ="Jolene123"
+end
+user3 = User.find_or_create_by!(name: "Lionel Messi", username: "futbol_geek") do |user|
+  user.password ="test123"
+end
+
+movie = Movie.find_or_create_by!(id: 101, title: "Happy Feet", vote_average: 2.5, tmdb_id: 1234)
+
+viewing_party = ViewingParty.find_or_create_by!(
+  name: "Big Time Movie Party",
+  start_time: Time.now + 1.day,
+  end_time: Time.now + 1.day + 2.hours,
+  movie_id: movie.id,
+  movie_title: movie.title,
+  host_id: user1.id
+)
+
+ViewingPartyUser.find_or_create_by!(user: user1, viewing_party: viewing_party, host: true)
+ViewingPartyUser.find_or_create_by!(user: user2, viewing_party: viewing_party, host: false)
