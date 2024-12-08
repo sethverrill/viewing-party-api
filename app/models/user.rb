9010f5e44001e889bcaec149a620f2d7c4c8c963
sethverrill <verrill.seth@gmail.com>
2024-12-08 10:46:13 -0700

    Update User model to include ViewingParty relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   validates :password, presence: { require: true }
   has_secure_password
   has_secure_token :api_key
-
+  has_many :viewing_party_users
+  has_many :hosted_parties, -> { ViewingPartyUser.hosts }, through: :viewing_party_users, source: :viewing_party
+  has_many :invited_parties, -> { ViewingPartyUser.invitees }, through: :viewing_party_users, source: :viewing_party
 end
