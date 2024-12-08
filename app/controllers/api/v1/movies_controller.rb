2c39c2867b00d44cbaca4df6b10f13191c0acc69
sethverrill <verrill.seth@gmail.com>
2024-12-08 10:44:23 -0700

    Refactor controller to removing connection logic and serializer logic

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,28 +1,13 @@
 class Api::V1::MoviesController < ApplicationController
   def index
-    api = Rails.application.credentials.dig(:tmdb, :api_read_access_token)
     query = params[:query]
+    response = if query.present?
+                TmdbService.serach_movies(query)
+              else
+                TmdbService.get_top_rated_movies
+              end
 
-    conn = Faraday.new(url: "https://api.themoviedb.org/3") do |f|
-      f.headers['Authorization'] = "Bearer #{api}"
-      f.headers['accept'] = 'application/json'
-    end
-
-    if query.present?
-      response = conn.get("search/movie", {
-        query: query,
-        include_adult: false,
-        language: 'en-US',
-        page: 1
-      })
-    else
-      response = conn.get("movie/top_rated", {
-        language: 'en-US',
-        page: 1
-      })
-    end
-
-    if response.success?
+    if response.status == 200
       json = JSON.parse(response.body, symbolize_names: true)
       movies = json[:results].first(20)
       render json: MovieSerializer.new(movies).serializable_hash.to_json
@@ -30,43 +15,13 @@ class Api::V1::MoviesController < ApplicationController
       render json: { error: "Failed to fetch movies: #{response.status}" }, status: :bad_request
     end
   end
+
+  def show
+    movie_data = TmdbService.get_movie(params[:id])
+    if movie_data.nil?
+      render json: { error: "Movie not found" }, status: :not_found
+    else
+      render json: movie_data, status: :ok
+    end
+  end
 end
-#   before_action :tmdb_connection
-#   def index
-#     response = @conn.get('movie/top_rated', language: 'en-US', page: 1)
-
-#     if response.success?
-#       json = JSON.parse(response.body, symbolize_names: true)
-#       movies = json[:results].first(20)
-#       render json: MovieSerializer.new(movies).serializable_hash.to_json
-#     else
-#       render json: { error: 'Failed to fetch top-rated movies' }, status: response.status
-#     end
-#   end
-
-#   def search
-#     query = params[:query]
-
-#     response = @conn.get('search/movie', {
-#       query: query,
-#       include_adult: false,
-#       language: 'en-US',
-#       page: 1
-#     })
-
-#     json = JSON.parse(response.body, symbolize_names: true)
-#     movies = json[:results].first(20)
-#     render json: MovieSerializer.new(movies).serializable_hash.to_json
-#   end
-
-#   private
-#   def tmdb_connection
-#     api = Rails.application.credentials.dig(:tmdb, :api_read_access_token)
-#     @conn = Faraday.new(url: "https://api.themoviedb.org/3") do |f|
-#       f.headers['Authorization'] = "Bearer #{api}"
-#       f.headers['accept'] = 'application/json'
-#     end
-#   end
-# end
-
-
